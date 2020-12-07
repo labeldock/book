01dfcd6470bd98ad369b0a9f423804d7e08b7534
AHN HO JUNG <labeldock@me.com>
2020-12-07 15:00:25 +0900

    update

---
+++ raspi/kiosk/kiosk-purge-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Clearing out unnecessary packages..."
+sudo apt-get remove --purge wolfram-engine scratch nuscratch sonic-pi idle3 smartsim java-common minecraft-pi python-minecraftpi python3-minecraftpi libreoffice python3-thonny geany claws-mail bluej greenfoot
+sudo apt-get autoremove
+
+echo "Making sure everything is up to date..."
+sudo apt-get update
+sudo apt-get upgrade
+
+echo "Installning necessary packages..."
+sudo apt-get install chromium-browser unclutter lxde -y
